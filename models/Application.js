9d3b45d3524e6a2dc366312569b849f59aa94b50
srd090 <ehospitalinfo2@gmail.com>
2025-06-23 06:12:11 +0200

    Update Application.js

--- models/Application.js
+++ models/Application.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const applicationSchema = new mongoose.Schema({
   idNumber: String,
   phoneNumber: String,
-  newphoneNumber: String,
   submittedAt: { type: Date, default: Date.now }
 });
 
